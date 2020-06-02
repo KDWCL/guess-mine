0ded9c46c28c044f0ed49baf962d2bcb88ac1dee
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-06-02 10:50:16 +0900

    토큰 추적

--- src/socketController.js
+++ src/socketController.js
@@ -1,15 +1,21 @@
 import events from "./events";
 
+let sockets = [];
+
 export default (socket) => {
   console.log(socket.id);
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
+    sockets.push({ id: socket.id, points: 0, nickname: nickname });
     broadcast(events.newUser, { nickname });
   });
   socket.on(events.disconnect, () => {
     // 연결되어진 클라이언트(브라우저)를 닫으면 console창에 disconnect 출력
     // console.log("disconnect");
+
+    // fillter이용하여 disconnect되어진 socket만 빼고 sockets에 새로운 배열을 만들어 넣는다.
+    sockets = sockets.filter((aSocket) => aSocket.nickname !== socket.nickname);
     broadcast(events.disconnected, { nickname: socket.nickname });
   });
 
@@ -32,3 +38,5 @@ export default (socket) => {
     broadcast(events.filled, { color });
   });
 };
+
+setInterval(() => console.log(sockets), 3000);
