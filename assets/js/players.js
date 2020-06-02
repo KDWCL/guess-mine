fdb3c8b93ceafd6bf262248b1ef176915968f1f9
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-06-02 11:44:38 +0900

    접속한 유저 이름 appenChild을 통해 화면 출력

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,3 +1,14 @@
+const board = document.querySelector("#jsPBoard");
+
+const addPlayers = (players) => {
+  board.innerHTML = ""; // jsPBoard안에 요소들을 한번 싹 지워준다.
+  players.forEach((player) => {
+    const playerElement = document.createElement("span");
+    playerElement.innerText = `${player.nickname}: ${player.points}`;
+    board.appendChild(playerElement);
+  });
+};
+
 export const handlePlayerUpdate = ({ sockets }) => {
-  console.log(sockets);
+  addPlayers(sockets);
 };
